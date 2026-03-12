2a1665db68b0ab10366a88b41e5c68b77072c332
Novel Builder <noreply@github.com>
2026-03-12 19:37:16 +0000

    validator: add pov_character to known outline keys

--- novel_builder/validator.py
+++ novel_builder/validator.py
@@ -118,7 +118,7 @@ def _iget(d, *keys):
 # ---------------------------------------------------------------------------
 
 _KNOWN_OUTLINE_KEYS = {
-    "story_title", "overall_arc", "chapters", "style_directives",
+    "story_title", "overall_arc", "pov_character", "chapters", "style_directives",
     "anti_patterns", "narrative_hooks", "world", "characters",
     "heritage", "setting", "locations",
 }
